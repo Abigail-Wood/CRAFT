4b48be4115ed6d3204c271a1cee8b9e39bf69187
Abigail Wood <acjwd3@gmail.com>
2019-03-12 20:33:28 +0000

    Change test_snptest_abf_cm input and output filenames

--- test/test_snptest_abf_cm.sh
+++ test/test_snptest_abf_cm.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-python craft.py --file "test/snptest_data/chr1.snptest.maf0.01.out" --file_type snptest --alpha 5e-5 --define_region cm --distance 0.1 --out snptest_cm.test.craft --outsf snptest_cm_abf.test.craft
+python craft.py --file "test/snptest_data/chr1.snptest.maf0.01.out" --file_type snptest --alpha 5e-5 --define_region cm --distance 0.1 --out test.snptest_cm.index --outsf test.snptest_cm_abf --no-finemap
