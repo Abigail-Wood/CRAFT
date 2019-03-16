b0a16949b260c85e5df47debe41aa6e523f0c641
Abigail Wood <acjwd3@gmail.com>
2019-03-16 10:46:53 +0000

    Remove additional abf test script as testing functionality now in main test scripts.

--- test/test_snptest_abf_cm.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-python craft.py --file "test/snptest_data/chr1.snptest.maf0.01.out" --file_type snptest --alpha 5e-5 --define_region cm --distance 0.1 --out test.snptest_cm.index --outsf test.snptest_cm_abf --no-finemap
