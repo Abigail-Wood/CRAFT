bdef429dd0cbb436943c28c7f60fd48ed87ebf7f
Abigail Wood <acjwd3@gmail.com>
2019-03-16 10:46:16 +0000

    Remove abf validation script (now works in basic test script)

--- test/test_abfvalidation.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-python craft.py --file "test/abf_validation_data/input.csv.csv" --file_type snptest --alpha 5e-5 --define_region cm --distance 0.1 --out snptest_cm.test.craft --outsf snptest_cm_abf.test.craft
