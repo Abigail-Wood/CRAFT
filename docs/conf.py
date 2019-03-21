5c68d905f16516226ba544bebb3c717df90b658e
Abigail Wood <acjwd3@gmail.com>
2019-03-21 18:39:06 +0000

    Change mock_import for vcf to resolve readthedocs build error

--- docs/conf.py
+++ docs/conf.py
@@ -15,7 +15,7 @@
 import os
 import sys
 sys.path.insert(0, os.path.abspath('..'))
-autodoc_mock_imports = ['vcf']
+autodoc_mock_imports = ['annotate']
 import craft.abf
 import craft.annotate
 import craft.config
