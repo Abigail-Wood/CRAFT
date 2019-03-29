b0afd72c99081fe32892a1c8f109f7886b5943dd
Abigail Wood <acjwd3@gmail.com>
2019-03-29 18:57:06 +0000

    Add test data for Plink using freq.cc file

--- test/test_plink_cm.sh
+++ test/test_plink_cm.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-python -m craft --file "test/plink_data/chr1.plink" --type plink2 --alpha 5e-5 --distance_unit cm --distance 0.1 --out output/test.plink_cm.index --outsf output/test.plink_cm.cred
+python -m craft --file "test/PLINK_test/Immunochip_PsA_ch1_PLINK.assoc.logistic" --type plink --frq frq_file.frq --alpha 5e-5 --distance_unit cm --distance 0.1 --out output/test.plink_cm_immunochip_ch1.index --outsf output/test.plink_cm_immunochip_ch1.cred
