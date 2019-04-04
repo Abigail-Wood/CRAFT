6200f6c0452ef026bea66356187bcd0b3175783e
Abigail Wood <acjwd3@gmail.com>
2019-04-04 15:24:06 +0100

    Remove old PLINK test data.

--- test/test_plink_freq_basic.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-python -m craft --file "test/basic.assoc.logistic" --type plink --frq "test/basic.frq.cc" --alpha 5e-5 --distance_unit cm --distance 0.1 --out output/test.plink_cm_immunochip_ch1.index --outsf output/test.plink_cm_immunochip_ch1.cred
