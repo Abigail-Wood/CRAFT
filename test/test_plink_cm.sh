214099540d02925f3fbc8a4306ee265ad64ac9a1
Abigail Wood <acjwd3@gmail.com>
2019-04-04 15:30:50 +0100

    Add new plink test scripts for cm + abf and cm + abf + finemap.

--- test/test_plink_cm.sh
+++ test/test_plink_cm.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-python -m craft --file "test/PLINK_test/Immunochip_PsA_ch1_PLINK.assoc.logistic" --type plink --frq frq_file.frq --alpha 5e-5 --distance_unit cm --distance 0.1 --out output/test.plink_cm_immunochip_ch1.index --outsf output/test.plink_cm_immunochip_ch1.cred
+python -m craft --file "test/plink_data/basic.assoc.logistic" --type plink --frq "test/plink_data/basic.frq.cc" --alpha 5e-5 --distance_unit cm --distance 0.1 --out output/test.plink_cm_immunochip_ch1.index --outsf output/test.plink_cm_immunochip_ch1.cred
