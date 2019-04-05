77bb48afc1f8f7a57d353d67b58e827bccff3145
Abigail Wood <acjwd3@gmail.com>
2019-04-05 14:33:23 +0100

    Remove scaling from ABF function; still believe this output is not valid / contains bugs.

--- craft/abf.py
+++ craft/abf.py
@@ -4,7 +4,7 @@ import pandas as pd
 from scipy.stats import norm
 
 def calc_abf(pval, maf, n, n_controls, n_cases):
-    """ Calculate Approximate Bayes Factor.
+    """Calculate Approximate Bayes Factor.
 
         (Wakefield, 2009, Genet Epidemiol.)
         Based on Chris Wallace work
@@ -16,6 +16,14 @@ def calc_abf(pval, maf, n, n_controls, n_cases):
         n_cases: Number of cases
     Returns:
         ABF
+
+    **Usage**
+    For a binary trait as no exact Bayes factor is calculable.
+    For case-control studies only.
+
+    **Limitations**
+    Assumes causal variant is included in SNP population (i.e. requires relatively densely genotyped SNPs.)
+    Assumes single causal variant only.
     """
 
     # Assert/set types
@@ -25,29 +33,32 @@ def calc_abf(pval, maf, n, n_controls, n_cases):
     n_controls = int(n_controls)
     n_cases = int(n_cases)
 
+    # Variant of ABF calculation that uses p-values
     # Calculate Z-score
     z = np.absolute(norm.ppf(pval / 2))
 
-    # Multiplicative model
+    # Multiplicative model - assumes if 2 copies, 'twice as much' risk if have one copy.
     x0 = 0
     x1 = 1
     x2 = 2
-    scale0 = n_controls
-    scale1 = n_cases
 
     # No idea what this does
-    d2 = (1-maf)**2 * x0**2 + 2*maf*(1-maf)*x1 + maf**2 * x2**2
     d1 = (1-maf)**2 * x0 + 2*maf*(1-maf)*x1 + maf**2 * x2
+    d2 = (1-maf)**2 * x0**2 + 2*maf*(1-maf)*x1 + maf**2 * x2**2
     V = (n_controls + n_cases) / (n_controls * n_cases * (d2-d1**2))
 
-    # Scale V (by assignment of scaling parameters this is always 1)
-    scale = ((n_controls + n_cases)/(scale0 + scale1)) * (scale0/n_controls) * (scale1/n_cases)
-    V = V * scale
-
     # Compute W (no idea what this is either)
+    # assumption: relative risk = 1.5
+    # null hypothesis is that relative risk = 1
+    # ppf = how many standard deviations is 99%
     W = (np.log(1.5) / norm.ppf(0.99))**2
     VW = V + W # simplification for ABF equation
-    ABF = np.sqrt(VW/V) * np.exp(- z**2 * W / (2 * VW)) # 2 ln ABF, see Kass & Raftery 1995
+
+    # Wakefield's approximate Bayes factor calculation (2009)
+    ABF = np.sqrt(VW/V) * np.exp(- z**2 * W / (2 * VW))
+
+    # Kass & Raftery (1995): 2 ln ABF allows comparison / rough interpretation of ABF meaning.
+    # ln_2_ABF = 2 * np.log(np.sqrt(VW/V) * np.exp(- z**2 * W / (2 * VW)))
 
     return ABF
 
@@ -78,9 +89,9 @@ def abf(data_dfs, cred_threshold):
         data = calc_postprobsum(data)
         data = data.sort_values('postprob_cumsum', ascending=False)
     # Trim credible SNPs based on posterior probability threshold
-        if cred_threshold == '95':
-            data = data[data.postprob_cumsum < 0.95]
-        if cred_threshold =='99':
-            data = data[data.postprob_cumsum < 0.99]
+    #    if cred_threshold == '95':
+    #        data = data[data.postprob_cumsum < 0.95]
+    #    if cred_threshold =='99':
+    #        data = data[data.postprob_cumsum < 0.99]
         data_list.append(data)
     return data_list
