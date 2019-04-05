d330e96407a9e1490e654f5e045e01284d120c82
Abigail Wood <acjwd3@gmail.com>
2019-04-05 14:36:14 +0100

    Edit main to use abf_beta rather than abf calculations.

--- craft/main.py
+++ craft/main.py
@@ -9,7 +9,7 @@ import glob
 
 import pandas as pd
 
-from craft import abf
+from craft import abf_beta
 from craft import annotate
 from craft import config
 from craft import log
@@ -97,7 +97,7 @@ def main():
         data_dfs = gs.get_locus_snps(stat_df, index_df, options.distance_unit)
 
     # Calculate ABF and posterior probabilities
-    data_list = abf.abf(data_dfs, options.cred_threshold)
+    data_list = abf_beta.abf(data_dfs, options.cred_threshold)
     data = pd.concat(data_list)
 
     # Annotate credible SNP set
