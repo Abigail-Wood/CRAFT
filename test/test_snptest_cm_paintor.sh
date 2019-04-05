0dde405559baf50e70339179f987b4eb2c182362
Abigail Wood <acjwd3@gmail.com>
2019-04-05 14:58:41 +0100

    Edit output file names to reflect test file source

--- test/test_snptest_cm_paintor.sh
+++ test/test_snptest_cm_paintor.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-python -m craft --file "test/snptest_data/chr1.snptest.maf0.01.out" --type snptest --alpha 5e-5 --distance_unit cm --distance 0.1 --out output/test.snptest_cm.index --outsf output/test.snptest_cm.cred --finemap_tool paintor
+python -m craft --file "test/snptest_data/chr1.snptest.maf0.01.out" --type snptest --alpha 5e-5 --distance_unit cm --distance 0.1 --out output/test.snptest_cm_paintor.index --outsf output/test.snptest_cm_paintor.cred --finemap_tool paintor
