77996f99f75232f445f16a8f17873dd0a94a94cd
Abigail Wood <acjwd3@gmail.com>
2019-04-09 14:30:39 +0100

    Add output for test script for Chromosome 1 of PsA Immunochip data

--- test/test_plink_imchip_cm.sh
+++ test/test_plink_imchip_cm.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-python -m craft --file "test/psa_ichp_test/Immunochip_PsA_PhaseIII_gencall_QC_pca_corr_hg19_PC1-PC2_chr1.assoc.logistic" --type plink --frq "test/psa_ichp_test/Immunochip_PsA_PhaseIII_gencall_QC_hg19_updateNames.frq.cc" --alpha 5e-5 --distance_unit cm --distance 0.1 --out output/test.plink_cm_immunochip_ch1.index --outsf output/test.plink_cm_immunochip_ch1.cred
+python -m craft --file "test/psa_ichp_test/Immunochip_PsA_PhaseIII_gencall_QC_pca_corr_hg19_PC1-PC2_chr1.assoc.logistic" --type plink --frq "test/psa_ichp_test/Immunochip_PsA_PhaseIII_gencall_QC_hg19_updateNames.frq.cc" --alpha 5e-5 --distance_unit cm --distance 0.1 --outdir output
