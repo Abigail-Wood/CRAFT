8b177100adc47633ceb311eae52d16d55d02f9a7
Abigail Wood <acjwd3@gmail.com>
2019-04-09 18:59:49 +0100

    Edit all test scripts to give output directory argument only.

--- test/test_plink_basic_cm.sh
+++ test/test_plink_basic_cm.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-python -m craft --file "test/plink_data/basic.assoc.logistic" --type plink --frq "test/plink_data/basic.frq.cc" --alpha 5e-5 --distance_unit cm --distance 0.1 --out output/test.plink_cm_immunochip_ch1.index --outsf output/test.plink_cm_immunochip_ch1.cred
+python -m craft --file "test/plink_data/basic.assoc.logistic" --type plink --frq "test/plink_data/basic.frq.cc" --alpha 5e-5 --distance_unit cm --distance 0.1 --outdir output
--- test/test_snptest_bp.sh
+++ test/test_snptest_bp.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 #make own output directory first in CRAFT folder
-python -m craft --file "test/snptest_data/chr1.snptest.maf0.01.out" --type snptest --alpha 5e-5 --distance_unit bp --distance 500000 --out output/test.snptest_bp.index --outsf output/test.snptest_bp.cred
+python -m craft --file "test/snptest_data/chr1.snptest.maf0.01.out" --type snptest --alpha 5e-5 --distance_unit bp --distance 500000 --outdir output
