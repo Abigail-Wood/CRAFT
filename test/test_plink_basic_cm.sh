e7ff0bda09fdbcf36f0f8f065b891c7dd46e8f0d
Abigail Wood <acjwd3@gmail.com>
2019-04-12 13:49:58 +0100

    Edit test scripts and data to provide full complement of testing for: abf, finemap, paintor; using cm and bp; multifile support; plink and snptest data.

--- test/test_plink_basic_cm.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-python -m craft --file "test/plink_data/basic.assoc.logistic" --type plink --frq "test/plink_data/basic.frq.cc" --alpha 5e-5 --distance_unit cm --distance 0.1 --outdir output
--- test/test_snptest_cm_finemap.sh
+++ test/test_snptest_cm_finemap.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-python -m craft --file "test/snptest_data/chr1.snptest.maf0.01.out" --type snptest --alpha 5e-5 --distance_unit cm --distance 0.1 --outdir output/ --finemap_tool finemap --n_causal_snps 1
+python -m craft --file "test/snptest_data/chr1.snptest.maf0.01.out" --type snptest --alpha 5e-5 --distance_unit cm --distance 0.1 --outdir output/ --finemap_tool finemap --n_causal_snps 3
